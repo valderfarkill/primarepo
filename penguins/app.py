b14acf6e48460d574438ef458ef6ba52807d3559
valderfarkill <salamoumayma@gmail.com>
2023-05-10 15:36:59 +0200

    penguins

---
+++ penguins/app.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+
+
+def main():
+    st.text("Ciao questo front-end funziona")
+    # slider
+    
+    st.write("la somma è ")
+
+
+if __name__ == "__main__":
+    main()
+
+#streamlit run app.py
